d6a23dbd058cc0e28083b66bb24eff4b9879acf6
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-10 11:52:04 +0300

    add DELETE request for individual drinks

--- drinks/views.py
+++ drinks/views.py
@@ -34,6 +34,6 @@ def drink_detail(request, id):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    
-    
+    elif request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
